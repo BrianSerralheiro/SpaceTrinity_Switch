77a2844de8078a380c7e195f9e5e65093a059be4
Arthur Tavares <arth_tavares@hotmail.com>
2019-09-22 18:23:13 -0300

    Added test script for the world selection menu(in SelectionTest scene)

---
+++ Assets/Scripts/WorldSelect.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WorldSelect : MonoBehaviour
+{
+	[SerializeField]
+	Graphic[] options;
+	[SerializeField]
+	int rowCount=3;
+	[SerializeField]
+	Graphic selector;
+	int selectionID;
+	void Start()
+    {
+        if(rowCount<2)rowCount=2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.UpArrow))selectionID-=rowCount;
+        if(Input.GetKeyDown(KeyCode.DownArrow))selectionID+=rowCount;
+        if(Input.GetKeyDown(KeyCode.RightArrow))selectionID++;
+        if(Input.GetKeyDown(KeyCode.LeftArrow))selectionID--;
+		if(selectionID>=options.Length)selectionID-=options.Length;
+		if(selectionID<0)selectionID+=options.Length;
+
+		if(options[selectionID])
+		{
+			selector.rectTransform.position=options[selectionID].rectTransform.position;
+			selector.rectTransform.anchoredPosition=options[selectionID].rectTransform.anchoredPosition;
+			selector.rectTransform.anchorMin=options[selectionID].rectTransform.anchorMin;
+			selector.rectTransform.anchorMax=options[selectionID].rectTransform.anchorMax;
+		}
+	}
+}
