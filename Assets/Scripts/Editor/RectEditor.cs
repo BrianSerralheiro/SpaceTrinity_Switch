639a959ce3afa965c2e1fa167af03bec21c90f3b
Arthur Tavares <arth_tavares@hotmail.com>
2019-09-23 17:13:09 -0300

    Added a helper to snap anchor to size for RectTransform(UI elements)

---
+++ Assets/Scripts/Editor/RectEditor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
+[CustomEditor(typeof(RectTransformHelper))]
+public class RectEditor : Editor
+{
+	public override void OnInspectorGUI()
+	{
+
+		
+		if(GUILayout.Button("Snap"))
+		{
+			RectTransform rect = (target as RectTransformHelper).GetComponent<RectTransform>();
+			Rect selfRect = rect.rect;
+			Rect parentRect = (rect.parent as RectTransform).rect;
+			rect.anchorMin=(new Vector2(rect.localPosition.x,rect.localPosition.y)+parentRect.size/2-rect.pivot*selfRect.size)/parentRect.size;
+			rect.anchorMax=rect.anchorMin+selfRect.size/parentRect.size;
+
+			rect.sizeDelta=Vector2.zero;
+			rect.anchoredPosition=Vector2.zero;
+		}
+	}
+}
