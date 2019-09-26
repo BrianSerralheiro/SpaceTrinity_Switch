26d5483672208dd59325df9c23a276d00aa86b63
Arthur Tavares <arth_tavares@hotmail.com>
2019-09-26 15:34:14 -0300

    Started the reworking of the spawning system to allow for separeted world to be configurated

---
+++ Assets/Scripts/Scriptable/EnemyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName ="Enemy",menuName ="Enemy Info")]
+public class EnemyInfo : ScriptableObject
+{
+	public Sprite[] sprites;
+	public System.Type GetScript()
+	{
+		return System.Type.GetType(name);
+	}
+}
---
+++ Assets/Scripts/Scriptable/WorldInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName ="World",menuName ="Wolrd Info")]
+public class WorldInfo : ScriptableObject
+{
+    public EnemyInfo[] enemies;
+	public Texture[] bgs;
+	public AudioClip Song;
+	public EnemyInfo Boss;
+	public AudioClip BossSong;
+
+}
