488e1c68b40d231d35394fc74d68fbf4a6044b36
Arthur Tavares <arth_tavares@hotmail.com>
2019-10-03 14:12:11 -0300

    Adde a base for the characther selection(left and right arrows), skin selection(up and down arrows, no visual indication) and a transition from the world selection(use space to select the world) to character selection

--- Assets/Scripts/WorldSelect.cs
+++ Assets/Scripts/WorldSelect.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
@@ -13,16 +11,39 @@ public class WorldSelect : MonoBehaviour
 	[SerializeField]
 	int rowCount=3;
 	[SerializeField]
+	GameObject charMenu;
+	[SerializeField]
 	Graphic selector;
 	int selectionID;
+	int movement;
 	void Start()
     {
         if(rowCount<2)rowCount=2;
     }
-
-    // Update is called once per frame
-    void Update()
+	private void OnEnable()
+	{
+		movement=-1;
+	}
+	void Update()
     {
+		if(movement==-1)
+		{
+			transform.Translate(-Screen.width*Time.deltaTime,0,0);
+			if(transform.position.x<Screen.width/2){
+				movement=0;
+				transform.position=new Vector3(Screen.width/2,Screen.height/2);
+			}
+			return;
+		}
+		if(movement==1)
+		{
+			transform.Translate(Screen.width*Time.deltaTime,0,0);
+			if(transform.position.x>Screen.width*1.5f){
+				gameObject.SetActive(false);
+				transform.position=new Vector3(Screen.width*1.5f,Screen.height/2);
+			}
+			return;
+		}
         if(Input.GetKeyDown(KeyCode.UpArrow))selectionID-=rowCount;
         if(Input.GetKeyDown(KeyCode.DownArrow))selectionID+=rowCount;
         if(Input.GetKeyDown(KeyCode.RightArrow))selectionID++;
@@ -37,11 +58,15 @@ public class WorldSelect : MonoBehaviour
 			selector.rectTransform.anchorMin=options[selectionID].rectTransform.anchorMin;
 			selector.rectTransform.anchorMax=options[selectionID].rectTransform.anchorMax;
 		}
-		if(Input.GetKeyDown(KeyCode.KeypadEnter)){
+		if(Input.GetKeyDown(KeyCode.Space)){
 			EnemySpawner.world=worlds[selectionID];
 			if(EnemySpawner.world)SceneManager.LoadSceneAsync("cen");
 			enabled=false;
 		}
-
+		if(Input.GetKeyDown(KeyCode.Q))
+		{
+			movement=1;
+			charMenu.SetActive(true);
+		}
 	}
 }
