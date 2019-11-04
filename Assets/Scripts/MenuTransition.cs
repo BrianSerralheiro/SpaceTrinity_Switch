623f58c7c4028532ae499e4de9f5bede483c44a0
Arthur Tavares <arth_tavares@hotmail.com>
2019-11-04 17:33:09 -0300

    Added chibi sprites without border, changed the horizontal transition of menus and croped the border for chibi

--- Assets/Scripts/MenuTransition.cs
+++ Assets/Scripts/MenuTransition.cs
@@ -1,20 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 [System.Serializable]
 public struct MenuTransition
 {
     public KeyCode key;
     public MenuSelect menu;
     public Vector3 flow;
-    public bool GetKeyDown()
-    {
+    public bool GetKeyDown(){
         return Input.GetKeyDown(key);
     }
     public void Open(){
-        menu?.Open(new Vector3(flow.x*18,flow.y*10));
+        float f=(float)Screen.width/(float)Screen.height;
+        menu?.Open(new Vector3(flow.x*f*10,flow.y*10));
     }
     public void Close(MenuSelect m){
-        m.Close(new Vector3(-flow.x*18,-flow.y*10));
+        float f=(float)Screen.width/(float)Screen.height;
+        m.Close(new Vector3(-flow.x*f*10,-flow.y*10));
     }
 }
