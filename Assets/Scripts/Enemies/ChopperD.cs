0a67f2f283599cf4cb1bd2ce3b7fdfab00a5a85f
Arthur Tavares <arth_tavares@hotmail.com>
2020-01-27 12:14:26 -0300

    Added new chopper that only dives down

---
+++ Assets/Scripts/Enemies/ChopperD.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+public class ChopperD : EnemyBase
+{
+    Helix helix;
+	public override void SetSprites(EnemyInfo ei){
+        hp=10;
+        GameObject go=new GameObject("Helix");
+        go.transform.parent=transform;
+        go.AddComponent<SpriteRenderer>().sprite=ei.sprites[1];
+        helix=go.AddComponent<Helix>();
+        go.AddComponent<BoxCollider2D>();
+        fallSpeed-=10;
+    }
+    new void Update()
+    {
+        if(Ship.paused)return;
+        base.Update();
+        SlowFall();
+    }
+    
+    protected override void Die(){
+        if(hp<=0){
+            helix.transform.parent=null;
+            Bullet b=helix.gameObject.AddComponent<Bullet>();
+            b.enabled=false;
+            b.damage=40;
+            b.owner="hel";
+            helix.time=Time.time+1;
+        }
+        base.Die();
+    }
+}
