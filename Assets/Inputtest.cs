a60e443a3b5709392063a564fd0778c845cc2a7e
Arthur Tavares <arth_tavares@hotmail.com>
2020-03-05 22:06:16 -0300

    Added keyboard input for both player axis, need configurate for controller input, use scene Input test to see what controller button has what id

---
+++ Assets/Inputtest.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Inputtest : MonoBehaviour
+{
+    KeyCode[] keys={
+        KeyCode.Joystick1Button0,
+        KeyCode.Joystick1Button1,
+        KeyCode.Joystick1Button2,
+        KeyCode.Joystick1Button3,
+        KeyCode.Joystick1Button4,
+        KeyCode.Joystick1Button5,
+        KeyCode.Joystick1Button6,
+        KeyCode.Joystick1Button7,
+        KeyCode.Joystick1Button8,
+        KeyCode.Joystick1Button9,
+        KeyCode.Joystick1Button10,
+        KeyCode.Joystick1Button11,
+        KeyCode.Joystick1Button12,
+        KeyCode.Joystick1Button13,
+        KeyCode.Joystick1Button14,
+        KeyCode.Joystick1Button15,
+        KeyCode.Joystick1Button16,
+        KeyCode.Joystick1Button17,
+        KeyCode.Joystick1Button18,
+        KeyCode.Joystick1Button19,
+
+    };
+    public Text tex;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.anyKeyDown){
+            foreach (KeyCode key in keys)
+            {
+                if(Input.GetKey(key))tex.text=key.ToString();
+            }
+        }
+    }
+}
