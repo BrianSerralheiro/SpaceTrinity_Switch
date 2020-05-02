9baf62c247c0483d26c36c67c25c0eabae3ab9fb
Brian Serralheiro <brian.serralheiro@gmail.com>
2020-05-02 09:13:40 -0800

    Fixed World 8 bug on enemy Slasher behaviour.

--- Assets/Scripts/Enemies/Slasher.cs
+++ Assets/Scripts/Enemies/Slasher.cs
@@ -2,14 +2,16 @@
 
 public class Slasher : EnemyBase {
 
-	private float timer = 3;
+	private float timer;
 	private Vector3 mod =new Vector3(0,-1f,0.1f);
 	static int shotID;
 	public override void SetSprites(EnemyInfo ei)
 	{
 		points = 120;
 		hp=90;
+		fallSpeed=-1;
 		shotID=ei.bulletsID[0];
+		timer=Time.time+2;
 		if(PlayerInput.Conected(1))hp=(int)(hp*ei.lifeproportion);
 	}
 
@@ -17,20 +19,20 @@ public class Slasher : EnemyBase {
 	{
 		if(Ship.paused) return;
 		base.Update();
-		timer-=Time.deltaTime;
-		if(timer>1){
+		if(timer>Time.time+1){
 			if(GetPlayer(transform.position).position.x<transform.position.x) transform.Translate(-Time.deltaTime,0,0);
 			else transform.Translate(Time.deltaTime,0,0);
 		}
-		if(transform.position.y>Scaler.sizeY/3) transform.Translate(0,-Time.deltaTime,0);
-		if(timer<=0)
+		SlowFall();
+		if(transform.position.y>Scaler.sizeY/3 && timer<Time.time)
 		{
-			timer=2;
 			Shoot();
 		}
+		fallSpeed=Mathf.MoveTowards(fallSpeed,-6,Time.deltaTime/5);
 	}
 	void Shoot()
 	{
+		timer=Time.time+2;
 		GameObject go = new GameObject("enemy");
 		go.AddComponent<SpriteRenderer>().sprite=Bullet.sprites[shotID];
 		go.AddComponent<BoxCollider2D>();
@@ -41,4 +43,13 @@ public class Slasher : EnemyBase {
 		go.transform.position=transform.position+mod;
 		//go.transform.localScale=Vector3.one*2;
 	}
+    void OnCollisionStay2D(Collision2D col)
+    {
+        Slasher slasher=col.gameObject.GetComponent<Slasher>();
+        if(slasher)
+        {
+            if(slasher.transform.position.x<transform.position.x)transform.Translate(Time.deltaTime,0,0,Space.World);
+            else transform.Translate(-Time.deltaTime,0,0,Space.World);
+        }
+    }
 }
