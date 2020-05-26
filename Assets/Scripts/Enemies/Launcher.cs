0e06223c04004ab59947c71db854e0f64610964c
Brian Serralheiro <brian.serralheiro@gmail.com>
2020-05-25 17:30:30 -0700

    Started messing with the Launcher particle effects.

--- Assets/Scripts/Enemies/Launcher.cs
+++ Assets/Scripts/Enemies/Launcher.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
 
-public class Launcher : EnemyBase {
+public class Launcher : EnemyBase 
+{
 	private float timer=5,spd;
 	private Transform rocket, burst,target;
 	private Vector3 rot,scale=Vector3.one, pos=new Vector3(0,0.4f,0.1f);
 	private Core core;
 	private static Sprite rocketSprite,burstSprite;
+	private int rocketTrail;
 	public override void SetSprites(EnemyInfo ei)
 	{
 		points = 100;
@@ -14,6 +16,7 @@ public class Launcher : EnemyBase {
 		timer=5;
 		rocketSprite=ei.sprites[1];
 		burstSprite=ei.sprites[2];
+		rocketTrail=ei.particleID[0];
 		GameObject go=new GameObject("core");
 		core=go.AddComponent<Core>().Set(ei.sprites[3],new Color(0.5f,0.1f,0.05f));
 		core.transform.parent=transform;
@@ -71,6 +74,7 @@ public class Launcher : EnemyBase {
 				v.Normalize();
 				if(timer>-4)rocket.Rotate(Vector3.Cross(v,rocket.up)*Time.deltaTime*270f*spd);
 				rocket.Translate(0,Time.deltaTime*8*spd,0);
+				ParticleManager.Emit(rocketTrail,burst.position,1);
 				if(rocket.position.x<-Scaler.sizeX/2-2 || rocket.position.x>Scaler.sizeX/2+2 || rocket.position.y<-Scaler.sizeY-2 || rocket.position.y>Scaler.sizeY+4)Destroy(rocket.gameObject);
 			}
 			else Create();
