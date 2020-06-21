6ed008efd1a1c5e486eb3d90a7fc9c134b554673
Arthur Tavares <arth_tavares@hotmail.com>
2020-06-20 22:06:33 -0300

    Added lights to city enviroment to be more eppealing, added  slight  camera movement based on ships position and added a "park" are to the ctty builder

---
+++ Assets/Scripts/Game/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    delegate void Del();
+    Del update;
+    void Start()
+    {
+        if(Ship.player2==-1)update=Single;
+        else update=Double;
+        Debug.Log(Ship.player2);
+    }
+    void Single(){
+        Vector3 v=transform.position;
+        v.x=Mathf.MoveTowards(v.x,EnemyBase.players[0].position.x/(Scaler.sizeX/4),Time.deltaTime);
+        transform.position=v;
+    }
+    void Double(){
+        Vector3 v=transform.position;
+        v.x=Mathf.MoveTowards(v.x,(EnemyBase.players[0].position.x-EnemyBase.players[1].position.x)/2/(Scaler.sizeX/4),Time.deltaTime);
+        transform.position=v;
+    }
+    void Update()
+    {
+        update?.Invoke();
+    }
+}
