2801df2400ba6fdbec5ce99bcb55efdf7b89b368
Arthur Tavares <arth_tavares@hotmail.com>
2020-08-02 20:14:13 -0300

    Script and scene for dialog testing

---
+++ Assets/DialogTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogTest : MonoBehaviour
+{
+    static bool reloaded;
+    public enum Enum1
+    {
+        Rose,Luna,Bruna,Tomoyo,Chinchua,Zantira
+    }
+    public enum Enum2
+    {
+        none,Rose,Luna,Bruna,Tomoyo,Chinchua,Zantira
+    }
+    public Enum1 player1;
+    public Enum2 player2;
+    void Update()
+    {
+        if(!reloaded)Loader.Scene("dialogtest");
+        reloaded=true;
+        Ship.player1=(int)player1;
+        Ship.player2=(int)player2-1;
+        enabled=false;
+    }
+}
