7c106755231d98a39c2e8e21ab0cf7c2451621f1
Arthur Tavares <arth_tavares@hotmail.com>
2020-08-07 15:35:00 -0300

    Added forest randomizer for world 3 trees

---
+++ Assets/Scripts/Game/TreePlanter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreePlanter : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] trees;
+    [SerializeField]
+    Vector2Int cellCount;
+    [SerializeField]
+    Vector2 gridArea;
+    [SerializeField][Range(0.1f,1)]
+    float maxOffset;
+    Vector2 offset;
+    void Start()
+    {
+        offset=new Vector2(gridArea.x/cellCount.x,gridArea.y/cellCount.y)*maxOffset;
+        Vector2 size=new Vector2(gridArea.x/cellCount.x,gridArea.y/cellCount.y);
+        Vector3 center=new Vector3(gridArea.x/2f,0,gridArea.y/2f);
+        for (int i = 0; i < cellCount.x; i++){
+            for (int j = 0; j < cellCount.y; j++){
+                Instantiate(trees[Random.Range(0,trees.Length)],transform).transform.localPosition=new Vector3(size.x*i+offset.x*Random.value,0,size.y*j+offset.y*Random.value)-center;
+            }
+        }
+        Destroy(this);
+    }
+}
