5d6a62f62f69149ba1e7b3487999e2be09614d5a
Brian Serralheiro <brian.serralheiro@gmail.com>
2020-08-15 11:43:07 -0700

    World 1 - Rose dialog test

--- Assets/DialogTest.cs
+++ Assets/DialogTest.cs
@@ -7,20 +7,21 @@ public class DialogTest : MonoBehaviour
     static bool reloaded;
     public enum Enum1
     {
-        Rose,Luna,Bruna,Tomoyo,Chinchua,Zantira
+        Rose,Luna,Bruna,Tomoyo,Chunhua,Zarina
     }
     public enum Enum2
     {
-        none,Rose,Luna,Bruna,Tomoyo,Chinchua,Zantira
+        none,Rose,Luna,Bruna,Tomoyo,Chunhua,Zarina
     }
     public Enum1 player1;
     public Enum2 player2;
-    void Update()
+    void Awake()
     {
         if(!reloaded)Loader.Scene("dialogtest");
         reloaded=true;
         Ship.player1=(int)player1;
         Ship.player2=(int)player2-1;
         enabled=false;
+        Debug.Log(Ship.player1);
     }
 }
